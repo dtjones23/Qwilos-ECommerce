874d4bfa18579cf0f974738aa03caaf7158955a1
DeAngelo Jones <deangelojones0708@gmail.com>
2024-03-23 11:47:09 -0500

    added initial files

---
+++ Ecommerce Store/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <p>Navbar</p>
+        
+      
+    </div>
+  )
+}
+
+export default Navbar
