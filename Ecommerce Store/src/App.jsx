93562e2ed7237721951cd99e8c73b9ad74bcb16d
DeAngelo Jones <deangelojones0708@gmail.com>
2024-03-24 17:13:35 -0500

    implemented links to App.jsx

--- Ecommerce Store/src/App.jsx
+++ Ecommerce Store/src/App.jsx
@@ -1,10 +1,29 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import ShopCategory from './Pages/ShopCategory'
+import Products from './Pages/Products';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import Shop from './Pages/Shop';
+
 
 function App() {
   return (
     <div>
+      <BrowserRouter >
       <Navbar />
+      <Routes >
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category = "men"/>}/>
+        <Route path='/womens' element={<ShopCategory category = "women"/>}/>
+        <Route path='/kids' element={<ShopCategory category = "kid"/>}/>
+        <Route path='/products' element={<Products/>}/>
+        <Route path='/products/:productId' element={<Products/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path = '/login' element={<LoginSignup/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
