714ccb89fd697bc08db4e1bca5d7250e926f4a21
DeAngelo Jones <deangelojones0708@gmail.com>
2024-03-27 13:15:29 -0500

    product display component is ready

--- Ecommerce Store/src/Components/ProductDisplay/ProductDisplay.jsx
+++ Ecommerce Store/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,13 +1,64 @@
-import React from 'react'
-import './ProductDisplay.css'
+import React from "react";
+import "./ProductDisplay.css";
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
 
 const ProductDisplay = (props) => {
-  const {product} = props
+  const { product } = props;
   return (
-    <div className='productdisplay'>
-      <img src={product.image} alt="" />
+    <div className="productdisplay">
+      <div className="productdisplay-left">
+        <div className="productdisplay-img-list">
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+        </div>
+        <div className="productdisplay-img">
+          <img className="productdisplay-main-img" src={product.image} alt="" />
+        </div>
+      </div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-star">
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_dull_icon} alt="" />
+          <p>(50)</p>
+          {/* this is placeholder for the number of ratings */}
+        </div>
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-oldprice">
+            ${product.old_price}
+          </div>
+          <div className="productdisplay-right-newprice">
+            ${product.new_price}
+          </div>
+        </div>
+        <div className="productdisplay-right-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+          accusamus eveniet, inventore voluptatum quae iure ipsum corporis et
+          animi sunt amet culpa quis voluptate. Animi molestiae eos qui veniam
+          pariatur?
+        </div>
+        <div className="productdisplay-right-size">
+          <h2>SelectSize</h2>
+          <div className="productdisplay-right-sizes">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+          <button>ADD TO CART</button>
+          <p className="productdisplay-right-category"> <span>Category :</span> Women , T-Shirt, Crop Top</p>
+          <p className="productdisplay-right-category"> <span>Tags :</span> Modern, Latest</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDisplay
+export default ProductDisplay;
