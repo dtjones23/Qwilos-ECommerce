f97a36ce5ab2ff00fdee6dd3183d5eb32f3497ff
DeAngelo Jones <deangelojones0708@gmail.com>
2024-04-06 15:00:06 -0500

    more bug fixes

--- Ecommerce Store/src/App.jsx
+++ Ecommerce Store/src/App.jsx
@@ -1,5 +1,4 @@
 
-import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import ShopCategory from './Pages/ShopCategory'
